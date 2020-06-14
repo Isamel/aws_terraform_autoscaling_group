da9eb0b282bef0c46f201ee6b4a94141e63bbe8d
unknown <Jose Salinas>
2020-06-13 22:48:44 -0700

    add main

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "null_resource" "depends_on" {
+    triggers = {
+        depends_on = join("", var.autoscaling_group_depends_on)
+    }
+}
+
+resource "aws_autoscaling_group" "autoscaling_group_terraform" {
+    count              = var.autoscaling_group_count
+    depends_on         = [null_resource.depends_on]
+    availability_zones = var.autoscaling_group_availability_zones
+    desired_capacity   = var.autoscaling_group_desired_capacity
+    max_size           = var.autoscaling_group_max_size
+    min_size           = var.autoscaling_group_min_size
+
+    launch_template {
+        id             = var.autoscaling_group_launch_template_id
+        version        = var.autoscaling_group_launch_template_version
+    }
+
+    tags               = local.tags
+}
